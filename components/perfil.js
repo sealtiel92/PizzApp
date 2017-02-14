27c1bebf087d2433c208eb5b2e5b779afa192656
sealtiel <sealtiel@x8bit.com>
2017-02-13 23:57:13 -0600

    add import Alert

--- components/perfil.js
+++ components/perfil.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { StyleSheet, Navigator, View, TouchableOpacity } from 'react-native';
+import { StyleSheet, Navigator, View, TouchableOpacity, Alert } from 'react-native';
 import { Container, Content, Header, Title, Icon, List, Button, ListItem, Text, InputGroup, Input } from 'native-base';
 
 const Menu = require('./menu');
